75da1cb705cbe36b0951f4a6908e1ba20e9cf22d
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-09 15:11:28 +0545

    div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import './App.css';
+import Navbar from './components/Navbar';
 
 const App = ()=> {
       return (
-        <div>hello</div>
+        <Navbar />
   );
 }
 
